c3e1ca23bed03eff2899d27f52f69bb509f685f7
Diego Caucota <diegocaucota@gmail.com>
2025-10-02 13:14:48 -0300

    del: files controllers routes

--- src/routes/index.routes.js
+++
@@ -1,8 +0,0 @@
-import productRoutes from "./products.routes.js"
-import { Router } from "express";
-
-const router = Router();
-
-router.use("/products", productRoutes);
-
-export default router;
--- src/routes/products.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createProduct, readAllProducts } from "../controllers/products.controllers.js";
-
-const router = Router();
-
-router.route('/').get(readAllProducts).post(createProduct);
-
-
-export default router;
